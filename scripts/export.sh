e862b8217c83dc0115aca12e16b7b0de693ba0f2
Krzysztof Knapik <knapsu@gmail.com>
2020-06-28 11:36:25 +0200

    Manage repository with a separate script

---
+++ scripts/export.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "${SCRIPT}")
+WORKDIR=${PWD}
+
+BUILD_DIR="app/build"
+REPO_DIR="repo"
+
+if [[ -z "${PLEX_TAG}" ]]; then
+  echo "Not a release. Skipping upload."
+  exit
+fi
+
+if [ ! -d "${BUILD_DIR}" ]; then
+  echo "Build directory not found"
+  exit
+fi
+
+echo "Add package to repository"
+FLATPAK_SUBJECT="Plex Media Player"
+FLATPAK_BRANCH="main"
+
+if [[ -n "${PLEX_TAG}" ]]; then
+  # Display version number in commit message
+  if [[ "${PLEX_TAG}" =~ ^v([0-9\.]+)\.([0-9]+)(-(.*))? ]]; then
+    FLATPAK_SUBJECT="Plex Media Player ${PLEX_TAG:1}"
+  else
+    FLATPAK_SUBJECT="Plex Media Player ${PLEX_TAG}"
+  fi
+  # Publish to release branch
+  FLATPAK_BRANCH="release"
+fi
+
+echo "commit: ${FLATPAK_SUBJECT}"
+echo "branch: ${FLATPAK_BRANCH}"
+
+# Sign commit
+flatpak build-export --subject="${FLATPAK_SUBJECT}" --gpg-sign=45971D9CA4780454 --update-appstream "${REPO_DIR}" "${BUILD_DIR}" "${FLATPAK_BRANCH}"
+
+flatpak repo --branches repo
+# flatpak repo --commits=app/tv.plex.PlexMediaPlayer/x86_64/${FLATPAK_BRANCH} repo
---
+++ scripts/repo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTDIR=$(dirname "${SCRIPT}")
+WORKDIR=${PWD}
+
+if [ $# -eq 0 ]; then
+  echo "Missing script arguments" >&2
+  exit 1
+fi
+OPTIONS=$(getopt -o '' -l download,upload -- "$@")
+if [ $? -ne 0 ] ; then
+  echo "Invalid script arguments" >&2
+  exit 2
+fi
+eval set -- "$OPTIONS"
+
+while true; do
+  case "$1" in
+    (--download)
+      REPO_DOWNLOAD="true"
+      shift;;
+    (--upload)
+      REPO_UPLOAD="true"
+      shift;;
+    (--)
+      shift; break;;
+    (*)
+      break;;
+  esac
+done
+
+REPO_DIR="repo"
+
+UPLOAD_USER=${UPLOAD_USER:?Missing user variable}
+UPLOAD_SERVER=${UPLOAD_SERVER:?Missing server variable}
+UPLOAD_PATH=${UPLOAD_PATH:?Missing path variable}
+
+if [[ "${REPO_DOWNLOAD}" == "true" ]]; then
+  echo "Download repository"
+  rsync --archive --delete --exclude '*.lock' --stats -e "ssh -i keys/id_rsa" ${UPLOAD_USER}@${UPLOAD_SERVER}:"${UPLOAD_PATH}/" "${REPO_DIR}/"
+fi
+
+if [[ "${REPO_UPLOAD}" == "true" ]]; then
+  echo "Upload repository"
+  rsync --archive --delete --exclude '*.lock' --stats -e "ssh -i keys/id_rsa" "${REPO_DIR}/" ${UPLOAD_USER}@${UPLOAD_SERVER}:"${UPLOAD_PATH}/"
+fi
