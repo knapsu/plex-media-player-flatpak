9e585dbe04f0f247c2ee358751a048f445a245ef
Krzysztof Knapik <knapsu@gmail.com>
2020-06-28 02:46:19 +0200

    Sign builds with PGP
    Additionally:
    - Show release version number in AppStream metadata
    - Ensure uchardet library is used
    - Manifest file cleanup

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,31 +25,32 @@ mkdir -p app
 # When building from tag the manifest file must be modified to match the source package
 if [[ -n "${PLEX_TAG}" ]]; then
   echo "Modifying manifest file to use ${PLEX_TAG} source"
-  cat tv.plex.PlexMediaPlayer.json | jq -r ".modules[].sources[0].tag=\"${PLEX_TAG}\"" > app/tv.plex.PlexMediaPlayer.json
+  cat tv.plex.PlexMediaPlayer.json | jq -r ".modules[1].sources[0].tag=\"${PLEX_TAG}\"" > app/tv.plex.PlexMediaPlayer.json
 else
   cp tv.plex.PlexMediaPlayer.json app/tv.plex.PlexMediaPlayer.json
 fi
 
 # AppStream metadata file
 # When building from tag ensure the metadata file contains release information
-# if [[ -n "${PLEX_TAG}" ]]; then
-#   echo "Modifying metadata file to include ${PLEX_TAG} release"
-#   # curl -f -s -S -o - https://api.github.com/repos/plexinc/plex-media-player/releases | jq -c ".[] | {tag_name, published_at}" | sed -E 's/\{"tag_name":"(.+)","published_at":"(.+)T.+"\}/<release version="\1" date="\2"\/>/'
-# else
-  cp tv.plex.PlexMediaPlayer.appdata.xml app/tv.plex.PlexMediaPlayer.appdata.xml
-# fi
-
-# Define package version string
-# When building from tag use number from its name
-# In all other situations use current date and commit hash
-FLATPAK_SUBJECT="Plex Media Player"
 if [[ -n "${PLEX_TAG}" ]]; then
-  FLATPAK_SUBJECT="Plex Media Player ${PLEX_TAG}"
-  if [[ "${VERSION}" =~ ^v[0-9]+ ]]; then
-    FLATPAK_SUBJECT="Plex Media Player ${VERSION:1}"
+  echo "Modifying metadata file to include ${PLEX_TAG} release"
+  cp tv.plex.PlexMediaPlayer.appdata.xml app/tv.plex.PlexMediaPlayer.appdata.xml
+  # curl -f -s -S -o - https://api.github.com/repos/plexinc/plex-media-player/releases | jq -c ".[] | {tag_name, published_at}" | sed -E 's/\{"tag_name":"(.+)","published_at":"(.+)T.+"\}/<release version="\1" date="\2"\/>/'
+  if [[ "${PLEX_TAG}" =~ ^v([0-9\.]+)\.([0-9]+)(-(.*))? ]]; then
+    VERSION="${PLEX_TAG:1}"
+  else
+    VERSION="${PLEX_TAG}"
   fi
+  sed -z -E "s|<releases>.*</releases>|<releases><release version=\"${VERSION}\"/></releases>|" -i app/tv.plex.PlexMediaPlayer.appdata.xml
+else
+  cp tv.plex.PlexMediaPlayer.appdata.xml app/tv.plex.PlexMediaPlayer.appdata.xml
+  sed -z -E "s|<releases>.*</releases>|<releases></releases>|" -i app/tv.plex.PlexMediaPlayer.appdata.xml
 fi
+
+# Desktop file
+cp tv.plex.PlexMediaPlayer.desktop app/tv.plex.PlexMediaPlayer.desktop
 
 # Create Flatpak
 cd app
-flatpak-builder --state-dir=../flatpak-builder --subject="${FLATPAK_SUBJECT}" build tv.plex.PlexMediaPlayer.json
+cat tv.plex.PlexMediaPlayer.json
+flatpak-builder --state-dir=../flatpak-builder --delete-build-dirs build tv.plex.PlexMediaPlayer.json
