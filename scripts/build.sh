b9751798d31863e589e9051daeb53818f362a45f
Krzysztof Knapik <knapsu@gmail.com>
2021-11-07 23:24:27 +0100

    Update releases data

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,19 +21,22 @@ fi
 rm -rf app
 mkdir -p app
 
-# Flatpak manifest file
-# When building from tag the manifest file must be modified to match the source package
 if [[ -n "${PLEX_TAG}" ]]; then
-  echo "Modifying manifest file to use ${PLEX_TAG} source"
+  echo "PLEX_TAG environment variable found. Using ${PLEX_TAG} sources"
+  cat tv.plex.PlexMediaPlayer.json | jq -r ".modules[1].sources[0].tag=\"${PLEX_TAG}\"" > app/tv.plex.PlexMediaPlayer.json
+fi
+
+# Flatpak manifest file contains source code information
+if [[ -n "${PLEX_TAG}" ]]; then
+  echo "Adjusting Flatpak manifest file"
   cat tv.plex.PlexMediaPlayer.json | jq -r ".modules[1].sources[0].tag=\"${PLEX_TAG}\"" > app/tv.plex.PlexMediaPlayer.json
 else
   cp tv.plex.PlexMediaPlayer.json app/tv.plex.PlexMediaPlayer.json
 fi
 
-# AppStream metadata file
-# When building from tag ensure the metadata file contains release information
+# AppStream metadata file contains release information
 if [[ -n "${PLEX_TAG}" ]]; then
-  echo "Modifying metadata file to include ${PLEX_TAG} release"
+  echo "Adjusting AppStream metadata file"
   cp tv.plex.PlexMediaPlayer.appdata.xml app/tv.plex.PlexMediaPlayer.appdata.xml
   # curl -f -s -S -o - https://api.github.com/repos/plexinc/plex-media-player/releases | jq -c ".[] | {tag_name, published_at}" | sed -E 's/\{"tag_name":"(.+)","published_at":"(.+)T.+"\}/<release version="\1" date="\2"\/>/'
   if [[ "${PLEX_TAG}" =~ ^v([0-9\.]+)\.([0-9]+)(-(.*))? ]]; then
